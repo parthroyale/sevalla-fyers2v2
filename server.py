bc058c780a69eba79cec28b8742f66209e21f88e
Your Name <you@example.com>
2025-02-11 12:52:01 +0530

    4

--- server.py
+++ server.py
@@ -1,7 +1,7 @@
 import os 
 
 data_dir = '/var/lib/data'
-data_dir = '/c/Users/acer/Documents/y2025/jan6/sevalla-fyers/data'
+# data_dir = '/c/Users/acer/Documents/y2025/jan6/sevalla-fyers/data'
 # check if data_dir exists
 if not os.path.exists(data_dir):
     print(f"Data directory {data_dir} does not exist.")
